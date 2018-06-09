e983c58e5ac745ea550b2946d0dd3a591b7f1d83
ZephV <zephkwok@outlook.com>
2018-06-09 16:20:04 +0800

    add pullDownRefresh

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,12 +6,29 @@ const weatherMap = {
   'heavyrain': '大雨',
   'snow': '雪'
 }
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
 Page({
   data: {
     nowTemp: '14°',
-    nowWeather: '多云'
+    nowWeather: '多云',
+    nowWeatherBackground: ''
+  },
+  onPullDownRefresh() {
+    this.getNow(() => {
+      wx.stopPullDownRefresh()
+    })
   },
   onLoad() {
+    this.getNow()
+  },
+  getNow(callback) {
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
       data: {
@@ -24,8 +41,16 @@ Page({
         let weather = result.now.weather;
         this.setData({
           nowTemp: temp + '°',
-          nowWeather: weatherMap[weather]
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/images/' + weather + '-bg.png'
         });
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather]
+        });
+      },
+      complete: () =>{
+        callback && callback();
       }
     })
   }
